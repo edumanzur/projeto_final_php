cc0d33ccd7205732f5e02165db899fe150e72e05
Lucas Moura <lsam.lucmoura0410@gmail.com>
2024-10-04 09:14:45 -0300

    Lucas

---
+++ code.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Site de criptografar mensagens.</h1>
+        <form action="get">
+            <label for="">Digite sua mensagen para ser encriptografada</label>
+            <input type="text" name="mensage_crip" id="">
+            <br><br>
+            <label for="">Digite sua mensagem criptografada:</label>
+            <input type="text" name="crip_mensage" id="">
+            <br>
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+    <?php
+        
+    ?>
+</body>
+
+</html>
